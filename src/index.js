b2d1ca3a2ca88655caf61bb930a21bd828fda403
Lorenzo Hiver <lorenzohiver@Lorenzos-MacBook-Pro.local>
2019-10-29 14:43:43 +0100

    init2

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ import { render } from 'react-dom';
 // == Import : local
 import App from 'src/components/App';
 
-
 // == Render
-// 1. Le composant racine (celui qui contient l'ensemble de l'app)
-const rootComponent = <App />;
-
-// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
-const target = document.getElementById('root');
+const rootComponent = (
+    <App />
+);
 
 // Le rendu de React => DOM
-render(rootComponent, target);
+render(rootComponent, document.getElementById('root'));
