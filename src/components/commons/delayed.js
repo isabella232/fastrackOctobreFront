d7259aabfba5b3d9b34b9381a67b2c54ad905793
Lorenzo Hiver <lorenzohiver@Lorenzos-MacBook-Pro.local>
2019-11-14 11:42:30 +0100

    fix/css + delayed composent

--- src/components/commons/delayed.js
+++ src/components/commons/delayed.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import axios from 'axios';
 
 import CardPartner from './cardPartner';
 
 const Delay = styled(CardPartner)`
-  border: 1px solid;
-  transition: opacity 1s cubic-bezier(0.86, 0, 0.07, 1);
+  transition: all 0.7s cubic-bezier(0.86, 0, 0.07, 1);
   opacity: 0;
+
+  &:hover {
+    transform: scale(1.04);
+    transition-duration: 500ms;
+    transition-property: transform;
+  }
 
   ${(props) => props.animate && `
     opacity: 1;
