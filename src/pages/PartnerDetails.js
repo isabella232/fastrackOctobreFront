be414aaef1a447873d3379d3cc453906c92eba1b
Damien Voindrot <lacouleur@MacBook-Pro-de-Damien.local>
2019-11-20 11:21:47 +0100

    chore/BasePartnerDisplay

--- src/pages/PartnerDetails.js
+++ src/pages/PartnerDetails.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router';
-import { getPartner } from '../services/client';
+// import { getPartner } from '../services/client';
+import styled from '@emotion/styled';
 
 import Nav from 'src/components/Nav';
-import TextHeader from 'src/components/TextHeader';
+import TextHeader from 'src/components/commons/TextHeader';
 import FixedButton from 'src/components/FixedButton';
+import Picture from 'src/components/commons/picture';
+import CardPartner from 'src/components/commons/cardPartner';
+import Container from 'src/components/commons/container';
+
+const Card = styled(CardPartner)`
+width : 80%;
+height : 100%;
+margin-top: 3rem; 
+position: relative; 
+`;
 
 
 const PartnerDetails = () => {
   const [partner, setPartner] = useState();
   const { partnerId } = useParams();
+  const baseUrl = "/src/styles/";
 
   useEffect(() => {
     axios.get(`https://fasttrack-octobre-back.herokuapp.com/api/partner/${partnerId}`)
@@ -19,14 +31,22 @@ const PartnerDetails = () => {
       });
   }, []);
 
+
+
   return (
     <>
       {partner
         && (
           <>
             <Nav />
-            <FixedButton />
             <TextHeader title={`${partner.firstName || 'toto'} ${partner.lastName}`} />
+            <FixedButton />
+            <Container>
+              <Card>
+                <Picture image={`${baseUrl}${partner.avatar}`} />
+                {console.log(partner.avatar)}
+              </Card>
+            </Container>
           </>
         )}
     </>
