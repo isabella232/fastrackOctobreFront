0882b0fd7367ffdc5a9a3cc1a1ab1acee288d3e3
Lorenzo Hiver <lorenzohiver@Lorenzos-MacBook-Pro.local>
2019-11-22 14:14:31 +0100

    core/addEmotionTheming

--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'emotion-theming';
 
 import store from './store';
 import PartnerDetails from './pages/PartnerDetails';
 import Home from './pages/Home';
-import css from '../src/styles/index.css';
+import { theme } from './services/theme';
+import css from './index.css';
 
 const rootComponent = (
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route path="/:partnerId" component={PartnerDetails} />
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/:partnerId" component={PartnerDetails} />
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   </Provider>
 
 );
