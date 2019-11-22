8877ce5021cb8fe909d3acb702aea19f62d1f7d4
mhulot <morgan.hulot@link-value.fr>
2019-11-22 10:06:35 +0100

    update project

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import store from 'src/store';
-import Home from 'src/pages/Home';
-import PartnerDetails from 'src/pages/PartnerDetails';
+import store from './store';
+import Nav from './components/Nav';
+import TextHeader from './components/TextHeader';
+import FixedButton from './components/FixedButton';
+import Card from './components/Card';
+
+const partner = {
+  firstname: 'test',
+  wait: 3000,
+};
 
 const rootComponent = (
   <Provider store={store}>
