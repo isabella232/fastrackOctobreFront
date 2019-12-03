e0729b21f3cac270ebf09d161ecc1cf9d8454c0f
Lorenzo Hiver <lorenzohiver@Lorenzos-MacBook-Pro.local>
2019-12-03 12:11:03 +0100

    fix/test

---
+++ src/components/commons/__tests__/fixedButton.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import serializer from 'jest-emotion';
+
+import EditButton from '../fixedButton';
+
+expect.addSnapshotSerializer(serializer);
+
+describe('fixedButtonCSS', () => {
+  it('renders with correct styles', () => {
+
+    const mock = {
+      colors: { white: '#111' },
+    };
+
+    const tree = renderer.create(<EditButton />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
