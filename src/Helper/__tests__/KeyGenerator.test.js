a96d759627cfcf28f583431c77a1de5475f49877
Lorenzo Hiver <lorenzohiver@Lorenzos-MacBook-Pro.local>
2020-01-03 15:14:42 +0100

    fixup! chore/keyGeneratorAndConvertToTime

--- src/Helper/__tests__/KeyGenerator.test.js
+++ src/Helper/__tests__/KeyGenerator.test.js
@@ -2,7 +2,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
 import * as emotion from 'emotion';
 import { createSerializer } from 'jest-emotion';
-import keyGenerator from '../keyGenerator';
+import keyGenerator from '../KeyGenerator';
 
 
 expect.addSnapshotSerializer(createSerializer(emotion));
