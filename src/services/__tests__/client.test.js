b1bdd1607d904bd48f8b8d5d16d06fcde313127c
Lorenzo Hiver <lorenzohiver@lorenzos-macbook-pro-1.home>
2020-01-06 14:17:00 +0100

    fixup! fixup! fixup! chore/keyGeneratorAndConvertToTime

--- src/services/__tests__/client.test.js
+++ src/services/__tests__/client.test.js
@@ -13,4 +13,28 @@ describe('client file', () => {
     axios.get.mockResolvedValue({ data: { test: 'coucou' } });
     expect(await partnerList()).toStrictEqual({ test: 'coucou' });
   });
+
+  it('should partnerReciever receive the data', async () => {
+    axios.get.mockResolvedValue(
+      { data: { name: 'lorenzo', experience: '50' } },
+    );
+    expect(await partnerReciever()).toStrictEqual(
+      {
+        convertedTime: { months: 2, years: 4 },
+        data: { name: 'lorenzo', experience: '50' },
+      },
+    );
+  });
+
+  it('should filtredPartnerList receive the data', async () => {
+    axios.get.mockResolvedValue({ data: { name: 'lorenzo' } });
+    expect(await filtredPartnerList()).toStrictEqual({ name: 'lorenzo' });
+  });
+
+  it('should dataSenger send the data', async () => {
+    const formData = jest.fn().mockResolvedValue('test');
+    await dataSender(formData);
+    console.log(axios.post);
+    expect(axios.post).toHaveBeenCalledWith(formData);
+  });
 });
