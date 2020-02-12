d0e17b7b00f5d0e143bb851db94af673ce83c2b7
Lorenzo Hiver <lorenzohiver@Lorenzos-MacBook-Pro.local>
2020-02-12 15:07:56 +0100

    fix/searchBar

--- src/services/configs/genralsConfigs.js
+++ src/services/configs/genralsConfigs.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const baseUrl = 'https://fasttrack-octobre-back.herokuapp.com/api';
+export const baseUrl = 'https://www.fasttrack-octobre-back.herokuapp.com/api';
